b57e997d083ff3b46d494704c2682f43402683f7
yan9woojin <yan9woojin@gmail.com>
2023-05-29 18:10:20 +0900

    feat: 배지 추가

--- js/main.js
+++ js/main.js
@@ -15,3 +15,22 @@ searchInputEl.addEventListener('blur', () => {
   searchInputEl.removeAttribute('placeholder');
   searchInputEl.value = '';
 });
+
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener(
+  'scroll',
+  _.throttle(() => {
+    if (window.scrollY > 500) {
+      gsap.to(badgeEl, 0.6, {
+        opacity: 0,
+        display: 'none',
+      });
+    } else {
+      gsap.to(badgeEl, 0.6, {
+        opacity: 1,
+        display: 'block',
+      });
+    }
+  }, 300),
+);
