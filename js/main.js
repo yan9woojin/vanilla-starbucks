246d314b24ce5392a51733da6a9f9abf9612e283
yan9woojin <yan9woojin@gmail.com>
2023-05-29 18:41:08 +0900

    feat: 비주얼 섹션 추가

--- js/main.js
+++ js/main.js
@@ -34,3 +34,12 @@ window.addEventListener(
     }
   }, 300),
 );
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+
+fadeEls.forEach((fadeEl, index) => {
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * 0.7,
+    opacity: 1,
+  });
+});
