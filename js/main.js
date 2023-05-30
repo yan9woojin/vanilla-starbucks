f65e083bdb2001261a4332d899a9961b57ee31b7
yan9woojin <yan9woojin@gmail.com>
2023-05-31 00:10:05 +0900

    feat: 푸터 추가

--- js/main.js
+++ js/main.js
@@ -115,3 +115,7 @@ spyEls.forEach((spyEl) => {
     .setClassToggle(spyEl, 'show')
     .addTo(new ScrollMagic.Controller());
 });
+
+const thisYear = document.querySelector('.this-year');
+
+thisYear.textContent = new Date().getFullYear();
